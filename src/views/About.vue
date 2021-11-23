cfcc897687e59ee607b56bdc273ec5c444d4024b
brunogomesww <bruno@waveweb2.com>
2021-11-23 16:09:15 +0000

    Add github

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,6 +3,11 @@
     <h2 class="mt-5">
       <a href="https://www.linkedin.com/in/brunovgggomes/" target="_blank">
         by Bruno Gomes
+      </a><br>
+    </h2>
+    <h2>
+      <a href="https://github.com/BrunoVGG/rules" target="_blank">
+        GitHub
       </a>
     </h2>
   </div>
